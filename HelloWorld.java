fe6aba662fea9b8bc28878efcef7be235cfeb827
omivan <ivan.omelchenk@gmail.com>
2022-04-25 20:21:04 +0100

    Update HelloWorld.java

--- HelloWorld.java
+++ HelloWorld.java
@@ -10,6 +10,6 @@ public class HelloWorld {
 		System.out.println("*    * *     *     *     *    *    *  *  * *   * * * * !");
 		System.out.println("*    * ***** ***** *****  ****     *   *   *   * *  ** !");
                                                           	                                                        
-   		// TODO Auto-generated method stub                  
+   		// TODO Auto-generated method stub         !         
 	}
 }
